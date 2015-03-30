766afb87e99a81be3747c4212df5a4fc99d7c639
dunderklumpen <dunderklumpen@dunderklumpen-ThinkPad>
2015-03-30 21:33:07 +0200

    s

--- Task_Scheduling_Helper/src/mainPackage/Main.java
+++ Task_Scheduling_Helper/src/mainPackage/Main.java
@@ -1,6 +1,13 @@
 package mainPackage;
 
+import guiPackage.GUI;
+
+import java.util.ArrayList;
+
 import javax.swing.UIManager;
+
+import objectPackage.Scheduler;
+import objectPackage.Task;
 
 /*
  * 	AUTHOR:	David Bennehag (David.Bennehag@Gmail.com)
@@ -11,11 +18,17 @@ import javax.swing.UIManager;
  * 		Create a number of tasks. Each task gets an execution time (C), a period (T) and (optionally) a deadline (D).
  * 			We then make a schedule with the selected scheduling algorithm (RMS, EDF...)
  * 
+ * 	FINISHED:
+ * 
  */
 
 public class Main
-{
+{	
+	private static ArrayList<Task> taskList = new ArrayList<Task>();
+	
+	static GUI gui = new GUI();
 
+	static Scheduler scheduler;
 	public static void main(String[] args)
 	{
 		//Set the look and feel of the program to be the same as system default
@@ -28,7 +41,35 @@ public class Main
 			e1.printStackTrace();
 		}
 
-		new GUI();
+		
+		
+		taskList.add(new Task("Task 1",100, 400));
+		taskList.add(new Task("Task 2", 200, 500));
+		taskList.add(new Task("Task 3", 300, 300));
+		taskList.add(new Task("Task 4", 300, 200));
+		
+		scheduler = new Scheduler(taskList);
+
+		for(Task task : taskList)
+		{
+			gui.addTaskToCB(task);
+		}
+	}
+	
+	public static ArrayList<Task> getTaskList()
+	{
+		return taskList;
+	}
+	
+	public static GUI getGui()
+	{
+		return gui;
+	}
+	public static Scheduler getScheduler()
+	{
+		return scheduler;
 	}
 
 }
+
+
